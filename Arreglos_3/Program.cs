3fd84443abc347a220ee9da058be0c5dac8cc1b3
Miurell Cristina <miurellcristina@hotmail.es>
2021-05-04 18:56:39 -0700

    Correccion de errores al imprimir promedios

--- Arreglos_3/Program.cs
+++ Arreglos_3/Program.cs
@@ -8,8 +8,72 @@ namespace Arreglos_3
 {
     class Program
     {
-        static void Main(string[] args)
+        //Declarar los dos vectores
+        private string[] nombre;
+        private float[] cal_quimica, cal_fisica;
+        private float promedioquimica, promediofisica;
+       
+        //Realizar metodo para cargar calificaciones
+        public void ingresardatos()
         {
+            string lineaCaptura;
+            cal_quimica = new float[5]; 
+            cal_fisica = new float[5];
+            nombre = new string[5];
+
+            Console.WriteLine("Regristro de calificaciones para Alumnos\n");
+            for (int i = 0; i<= 4; i++)
+            {
+                Console.WriteLine("Ingrese nombre del alumno No.  " + (i + 1) + ": ");
+                nombre[i] = Console.ReadLine();
+
+                Console.WriteLine("\nIngrese calificacion de Quimica: ");
+                lineaCaptura = Console.ReadLine();
+
+                cal_quimica[i] = float.Parse(lineaCaptura);
+                Console.WriteLine("\nIngrese calificacion de Fisica: ");
+                lineaCaptura = Console.ReadLine();
+                cal_fisica[i] = float.Parse(lineaCaptura);
+            }
+        }
+
+        //Metodo para calcular el promedio
+        public void calcularpromedio()
+        {
+            float total =0;
+            for (int i = 0; i <= 4; i++)
+            {
+                total = total + cal_quimica[i];
+            }
+            promedioquimica = total / 5;
+            total = 0;
+            for (int i = 0; i <= 4; i++)
+            {
+                total = total + cal_fisica[i];
+            }
+            promediofisica = total / 5;
+
+            Console.WriteLine("La materia que tuvo el mayor promedio general fue ");
+            if (promedioquimica > promediofisica)
+            {
+                Console.WriteLine("Quimica con " + promedioquimica);
+            }
+            else
+            {
+                Console.WriteLine("Fisica con " + promediofisica);
+            }
+            Console.ReadKey();
+        }
+
+       
+        static void Main(string[] args)
+        //Imprimir los metodos
+        {
+            Program calificaciones = new Program();
+            calificaciones.ingresardatos();
+            calificaciones.calcularpromedio();
+            
+            
         }
     }
 }
